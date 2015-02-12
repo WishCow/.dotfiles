d9638fa57b709488e3fcf660834ee5b49108d647
Norbert Kéri <norbert.keri@pearson.com>
2015-02-12 18:00:01 +0100

    Change file copies to be symlinks

--- roles/shell/files/remote/apps/dircolors.sh
+++ roles/shell/files/remote/apps/dircolors.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Trapd00r's dircolors
-eval $(dircolors -b "$DIR"/lscolors/LS_COLORS)
+eval $(dircolors -b ~/.lscolors/LS_COLORS)
--- roles/shell/files/remote/main.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-. "$DIR/variables.sh"
-for f in "$DIR"/apps/*.sh; do
-    . "$f"
-done
