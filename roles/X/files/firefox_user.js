6db35186997ce404ac185e60ae6187aae23bdd95
Norbert Kéri <keri.norbert.miklos@gmail.com>
2015-06-07 12:35:07 +0200

    Add firefox user.js

---
+++ roles/X/files/firefox_user.js
@@ -0,0 +1,27 @@
+// Pocket
+user_pref("browser.pocket.api", "0.0.0.0");
+user_pref("browser.pocket.site", "0.0.0.0");
+user_pref("browser.pocket.enabled", false);
+user_pref("browser.toolbarbuttons.introduced.pocket-button", false);
+user_pref("browser.pocket.oAuthConsumerKey", "asdf");
+
+// Move cache to ram
+user_pref("browser.cache.disk.enable", false);
+user_pref("browser.cache.memory.enable", true);
+
+// Some social crap
+user_pref("loop.enabled", false);
+
+
+// VimFX hotkeys
+user_pref("extensions.VimFx.commands.follow_next.keys", '["e"]');
+user_pref("extensions.VimFx.commands.follow_previous.keys", '["q"]');
+user_pref("extensions.VimFx.commands.restore_tab.keys", '["u"]');
+user_pref("extensions.VimFx.commands.scroll_half_page_down.keys", '["c-f"]');
+user_pref("extensions.VimFx.commands.scroll_half_page_up.keys", '["c-b"]');
+user_pref("extensions.VimFx.commands.scroll_left.keys", "[]");
+user_pref("extensions.VimFx.commands.scroll_page_down.keys", "[]");
+user_pref("extensions.VimFx.commands.scroll_page_up.keys", "[]");
+user_pref("extensions.VimFx.commands.scroll_right.keys", "[]");
+user_pref("extensions.VimFx.commands.tab_next.keys", '["l"]');
+user_pref("extensions.VimFx.commands.tab_prev.keys", '["h"]');
